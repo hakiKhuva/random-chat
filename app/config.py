ed9acb09d75ca089c8be550e6f4e2771a12a7ed6
Harkishan Khuva <harkishankhuva02@gmail.com>
2023-02-12 23:53:11 +0530

    updated spelling of KEY

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 APP_NAME = "Random Chat"
 
 APP_CONFIG = {
-    "SECRET_KEY": os.environ.get("SECRET_KET", os.urandom(20)),
+    "SECRET_KEY": os.environ.get("SECRET_KEY", os.urandom(20)),
     "SQLALCHEMY_DATABASE_URI": os.environ.get("DATABASE_URI", "sqlite:///master.sqlite"),
     "STATIC_FOLDER": "static"
 }
