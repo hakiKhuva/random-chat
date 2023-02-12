7f91adefab8f0c7251cf345769dc5e56fc1c6a13
Harkishan Khuva <harkishankhuva02@gmail.com>
2023-02-13 00:10:06 +0530

    updated datatype of sid and message_to

--- app/models/chat_user.py
+++ app/models/chat_user.py
@@ -8,8 +8,8 @@ class ChatUser(Base):
     looking_age = db.Column(db.String(20), nullable=False)
     gender = db.Column(db.String(30), nullable=False)
     looking_gender = db.Column(db.String(30), nullable=False)
-    sid = db.Column(db.Text, nullable=False, unique=True)
-    message_to = db.Column(db.Text, nullable=True, unique=True)    
+    sid = db.Column(db.String(50), nullable=False, unique=True)
+    message_to = db.Column(db.String(50), nullable=True, unique=True)    
     status = db.Column(db.String(20), nullable=False, default="free")
 
     def __repr__(self) -> str:
